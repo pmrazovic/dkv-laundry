f2528ea31c0faab9d1bf7eabc77de27045331d17
Petar Mrazovic <petar.mrazovic@gmail.com>
2016-09-26 23:26:05 +0200

    bugfix

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -102,7 +102,7 @@ class SlotsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_slot
-      @slot = Slot.find(params[:id])
+      @slot = Slot.where(:id => params[:id]).first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
